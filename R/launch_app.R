8a3aa01e1ecee4be6a5cdacc1e2310f7c7017e15
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-09-24 12:11:43 +0200

    updated license and descrption

--- R/launch_app.R
+++ R/launch_app.R
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 #' @title Launch the DQA graphical user interface (GUI)
 #'
 #' @param port The port, the MIRACUM DQA Tool is running on (default: 3838)
