c6265d660e93c5536e4e176795e967599c9d0b42
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-11-05 16:25:55 +0100

    included download zip button to reporting tab

--- R/moduleReport.R
+++ R/moduleReport.R
@@ -1,4 +1,5 @@
-# DQAgui - A graphical user interface (GUI) to the functions implemented in the R package 'DQAstats'.
+# DQAgui - A graphical user interface (GUI) to the functions implemented in the
+# R package 'DQAstats'.
 # Copyright (C) 2019 Universitätsklinikum Erlangen
 #
 # This program is free software: you can redistribute it and/or modify
@@ -21,30 +22,124 @@
 #' @param output Shiny server output object
 #' @param session Shiny session object
 #' @param rv The global 'reactiveValues()' object, defined in server.R
-#' @param input_re The Shiny server input object, wrapped into a reactive expression: input_re = reactive({input})
+#' @param input_re The Shiny server input object, wrapped into a reactive
+#'   expression: input_re = reactive({input})
 #'
 #' @export
 #'
 # moduleReportServer
-moduleReportServer <- function(input, output, session, rv, input_re){
+moduleReportServer <- function(input,
+                               output,
+                               session,
+                               rv,
+                               input_re) {
 
   observe({
-    # wait here for flag to create report; this can be done, when everything we need for the report is there
+    # wait here for flag to create report; this can be done, when everything
+    # we need for the report is there
     req(rv$create_report)
 
     if (is.null(rv$report_created)){
-      DQAstats::createMarkdown_(rv = rv, utils = rv$utilspath, outdir = tempdir(), headless = rv$headless)
+      DQAstats::createMarkdown_(
+        rv = rv,
+        utils = rv$utilspath,
+        outdir = tempdir(),
+        headless = rv$headless
+      )
       rv$report_created <- TRUE
     }
   })
 
+  observe({
+    req(rv$report_created)
+
+    # create export dir
+    exportdir <- paste0(tempdir(), "/export/")
+    cat("\nCreate ", exportdir, "\n\n")
+    dir.create(exportdir)
+
+    # write files
+    # datamap
+    data.table::fwrite(
+      x = rv$datamap$target_data,
+      file = paste0(exportdir, "datamap_target.csv")
+    )
+    data.table::fwrite(
+      x = rv$datamap$source_data,
+      file = paste0(exportdir, "datamap_source.csv")
+    )
+
+    # completeness
+    data.table::fwrite(
+      x = rv$checks$etl,
+      file = paste0(exportdir, "etl_checks.csv")
+    )
+    data.table::fwrite(
+      x = rv$completeness,
+      file = paste0(exportdir, "completeness.csv")
+    )
+
+    # conformance
+    data.table::fwrite(
+      x = rv$checks$value_conformance,
+      file = paste0(exportdir, "value_conformance.csv")
+    )
+  })
+
 
   output$download_report <- downloadHandler(
-    filename = function(){paste0("DQA_report_", gsub("\\-|\\:| ", "", substr(rv$start.time, 1, 16)), ".pdf")},
+    filename = function(){
+      paste0("DQA_report_",
+             gsub("\\-|\\:| ",
+                  "",
+                  substr(rv$start.time, 1, 16)),
+             ".pdf")
+    },
     content = function(file){
-      file.copy(paste0(tempdir(), "/DQA_report_", gsub("\\-|\\:| ", "", substr(rv$start.time, 1, 16)), ".pdf"), file)
+      file.copy(
+        paste0(tempdir(),
+               "/DQA_report_",
+               gsub("\\-|\\:| ",
+                    "",
+                    substr(rv$start.time, 1, 16)),
+               ".pdf"),
+        file
+      )
     },
     contentType = "application/pdf"
+  )
+
+
+  output$download_results <- downloadHandler(
+    filename = function() {
+      paste0("DQA_results_",
+             gsub("\\-|\\:| ",
+                  "",
+                  substr(rv$start.time, 1, 16)),
+             ".zip")
+    },
+    content = function(fname) {
+
+      # temporarily set tempdir as wd
+      oldwd <- getwd()
+      setwd(tempdir())
+      print(getwd())
+
+      exportdir <- paste0(tempdir(), "/export/")
+
+      # export files
+      utils::zip(
+        zipfile = fname,
+        files = c(
+          paste0("export/",
+                 list.files(exportdir))
+        ))
+
+      # return to old wd
+      setwd(oldwd)
+      print(getwd())
+    },
+    contentType = "application/zip"
   )
 }
 
@@ -55,16 +150,38 @@ moduleReportServer <- function(input, output, session, rv, input_re){
 #' @export
 #'
 # moduleReportUI
-moduleReportUI <- function(id){
+moduleReportUI <- function(id) {
   ns <- NS(id)
 
   tagList(
     fluidRow(
-      box(title = "Reporting",
-          downloadButton(ns("download_report"), "Download Report", style="white-space: normal; text-align:center;
-                                                                  padding: 9.5px 9.5px 9.5px 9.5px;
-                                                                  margin: 6px 10px 6px 10px;"),
-          width = 6
+      column(6,
+             box(
+               title = "Reporting",
+               downloadButton(
+                 ns("download_report"),
+                 "Download Report",
+                 style = paste0(
+                   "white-space: normal; ",
+                   "text-align:center; ",
+                   "padding: 9.5px 9.5px 9.5px 9.5px; ",
+                   "margin: 6px 10px 6px 10px;")),
+               width = 12
+             )
+      ),
+      column(6,
+             box(
+               title = "Results",
+               downloadButton(
+                 ns("download_results"),
+                 "Download Results (ZIP)",
+                 style = paste0(
+                   "white-space: normal; ",
+                   "text-align:center; ",
+                   "padding: 9.5px 9.5px 9.5px 9.5px; ",
+                   "margin: 6px 10px 6px 10px;")),
+               width = 12
+             )
       )
     )
   )
