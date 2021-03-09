786f874ab9eee776e26024306d5a9310de23faa9
Jonathan <jonathan.mang@uk-erlangen.de>
2021-03-09 11:19:52 +0100

    chore: starting to add restricting date functionality to the gui

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -28,7 +28,7 @@ my_desc$set_authors(c(
 # Remove some author fields
 my_desc$del("Maintainer")
 # Set the version
-my_desc$set_version("0.1.5")
+my_desc$set_version("0.1.5.9001")
 # The title of your package
 my_desc$set(Title = "DQA GUI")
 # The description of your package
@@ -84,7 +84,7 @@ usethis::use_package("processx", type = "Suggests")
 usethis::use_package("lintr", type = "Suggests")
 
 # Development package
-stats_tag <- "v0.1.6" # e.g. "v0.1.6" or "development"
+stats_tag <- "development" # e.g. "v0.1.6" or "development"
 devtools::install_git(
   url = "https://gitlab.miracum.org/miracum/dqa/dqastats.git",
   ref = stats_tag,
